18124f294177326a53610415db71ec3abe19cd13
klandestino7 <klandestino6@hotmail.com>
2024-05-16 17:09:15 -0300

    refact: moving code to another place

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,24 +4,17 @@ rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aw
 
 shared_scripts {
 	"lib/utils.lua",
-	"modules/utils/i18n.lua",
-	"modules/utils/dataview.lua",
+	"lib/i18n.lua",
+	"lib/dataview.lua",
 }
 
 client_scripts{
-	"modules/_main.lua",
 	"data/horses_components.lua",
 	"data/mp_overlay_layers.lua",
 	"data/mp_peds_components.lua",
 
-	"modules/appearance/constants.lua",
-	"modules/appearance/overlays.lua",
-	"modules/appearance/main.lua",
-	"modules/appearance/apparatusDatabase.lua",
-	"modules/appearance/applyPersonaAppearance.lua",
-	"modules/appearance/clothingSystemResolvers.lua",
-	"modules/appearance/utils.lua",
-	"modules/appearance/utils.js",
+	"modules/utils.lua",
+	"modules/utils.js",
 }
 
 files {
